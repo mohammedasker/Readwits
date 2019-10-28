98c889bc84da5cfaa14349317424a486c188bfe8
Mohammed Asker <zetoanime@gmail.com>
2019-10-28 15:45:47 +0300

    added fetch API code snippets & partially works

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,34 @@
-// Function for event listeners
-//(Fire an event when a user clicks button codes)
+// Constant variables for input search
+const QUERY = document.querySelector('search-input');
 
-// When a user clicks search button, start searching for the cintent
+// Event listeners for search bar and search button
+document.querySelector('#submit-button').addEventListener('click', handleResponse);
+
+// Initiate the fetch
+function handleResponse() {
+	fetch(`https://www.googleapis.com/books/v1/volumes?q=${QUERY}&callback=handleResponse`)
+      .then((response) => response.text())
+      .then((data) => {
+        //document.querySelector('output').innerHTML = data;
+        console.log(data)
+      })
+      .catch((error) => console.log(error))
+	}
+
 	/* 
-    Function for fetching API...
-    (URL request for the book codes)
-
 	Function for filtering relevant contents...
 	(GET the relevant contents codes)
+
+	fetch("https://www.googleapis.com/books/v1/volumes?q=");
+		.then((response) => response.json())
+		.then(data => {
+			console.log(data)
+		})
+		.catch(error => console.error(error))
 
 	Function for showing results...
 	(Display the results into an item lists code)
 	*/
+
+	//Reference only
+	//"https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse"
