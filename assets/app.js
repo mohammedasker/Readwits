ecc06fa5d74d949831f2e43839c6bdec79d2c1d0
Mohammed Asker <zetoanime@gmail.com>
2019-11-04 12:14:58 +0300

    created the layout of search results

--- assets/app.js
+++ assets/app.js
@@ -13,10 +13,14 @@ function searchBook() {
       	let output = "<h2>Search results</h2>";
       	data.items.forEach(book => {
       		output += `
+          <div class="results-box">
       		<ul>
-	      		<li>Title: ${book.volumeInfo.title}</li>
-	      		<li>Author: ${book.volumeInfo.authors}</li>
+            <img src= ${book.volumeInfo.imageLinks.smallThumbnail}>
+	      		<li><b>Title:</b> ${book.volumeInfo.title}</li>
+	      		<li><b>Author:</b> ${book.volumeInfo.authors}</li>
+            <a href=${book.volumeInfo.canonicalVolumeLink}><button class="book-info">See this book</button></a>
       		</ul>
+          </div>
       		`;
       	});
       	// Display the search result
@@ -26,34 +30,3 @@ function searchBook() {
       // Throw an error message when something went wrong during the search
       .catch((error) => console.log(error));
 	}
-
-      // Ajax method (Has the same undefined problem as above)
-      // When user inputs the search bar and clicked search button, start searching
-      /*
-      function searchBook() {
-      const query = document.querySelector('#search-input').value;
-      // Initiate the XMLHttpRequest call
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=${query}&callback=searchBook`, true);
-      // Check if status equals 200, then get the API data
-      xhr.onload = function() {
-        if (this.status == 200) {
-          var book = JSON.stringify(this.responseText);
-          // Loop the data result and display it as the list
-          var output = '';
-          for(var i in book){
-            output +=`
-          <ul>
-            <li>Title: ${book[i].title}</li>
-            <li>Author: ${book[i].authors}</li>
-          </ul>
-          `
-          }
-          // Show the search result
-          document.getElementById('output').innerHTML = output;
-          console.log(this.responseText);
-        }
-      }
-      xhr.send();
-    }
-    */
